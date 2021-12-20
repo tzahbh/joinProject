04c8bb5ff01efacbb95d2cae584d14dbbbb158ec
tzahbh <96420135+tzahbh@users.noreply.github.com>
2021-12-20 17:13:02 +0200

    init 3 end-points [ping, upload, view]

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,59 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path")
+const fs = require("fs");
+var fileupload = require("express-fileupload");
 var router = express.Router();
 
 const app = express();
 const PORT = 8888;
-
+const filesDic = "files";
 
 
 app.use(bodyParser.json());
 
 
-app.get("/", (req, res) => {
-  res.send("dev api status - up");
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
 });
+
+app.use(fileupload());
+
+
+app.put("/upload", (req, res) => {
+
+  if(!req.files.picture)
+    {
+        res.status(404).send("File was not found");
+        return;
+    }
+
+  const file = req.files.picture;
+  const fileName = file.name;
+  const filePath = path.resolve(`${filesDic}/${fileName}`)
+
+  if (fs.existsSync(filePath)) { 
+      res.status(400).send(`File Name ${fileName} Already Exists.`);
+  }
+  else{
+    file.mv(filePath, function(err) {
+      if (err)
+       return res.status(500).send(err);
+    });
+  }
+  res.status(201).send(`File Uploaded ${fileName}`);
+  });
+
+app.get("/view/:file_name", (req, res) => {
+  const fileName = req.params.file_name ? req.params.file_name : null;
+  const filePath = path.resolve(`${filesDic}/${fileName}`)
+  if (fs.existsSync(filePath)) { 
+      res.status(200).download(filePath);
+  }
+  else{
+      res.status(404).send(`${fileName} is not Found.`);
+  }
+})
 
 
 app.listen(PORT);
